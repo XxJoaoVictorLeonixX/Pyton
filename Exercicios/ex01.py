32e28fab8d95661dfe00d7eec3aaedbd721c161b
João Victor Leoni <joaovictorleoni2021@gmail.com>
2022-12-01 20:16:54 -0300

    .

---
+++ Exercicios/ex01.py
@@ -0,0 +1,4 @@
+dia = input('Informe o dia do seu aniversario:')
+mes = input('Informe o mes do seu aniversario:')
+ano = input('Informe o ano do seu aniversario:')
+print('Você nasceu no dia',dia, 'no mes de',mes, 'e no ano de',ano, 'correto?');
---
+++ Exercicios/ex03.py
@@ -0,0 +1,4 @@
+n1 = int(input('Informe o primeiro numero:'))
+n2 = int(input('Informe o segundo numero:'))
+soma = n1 + n2
+print('A soma de',n1, 'com',n2, 'é',soma,)
