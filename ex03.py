188847439830bfd473f3b8e46d1ba23526fba505
João Victor Leoni <joaovictorleoni2021@gmail.com>
2023-09-20 00:09:45 -0300

    .

---
+++ ex03.py
@@ -0,0 +1,19 @@
+x = 5 # x recebe o valor 5
+y = 3.5
+z = 1 + 2j
+
+w = "Ana"
+w = 'Ana'
+
+h,j,l = 1, 3, 5
+print(h)
+print(j)
+print(l)
+
+a = b = c = "Pyton"
+print(a)
+print(b)
+print(c)
+
+h = w + a
+print(h)
